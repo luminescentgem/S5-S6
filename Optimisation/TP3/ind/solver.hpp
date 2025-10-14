636a1827e2b99d1041617dd3c20958904bb89e97
Garice Bertolini <bertolini.garice@icloud.com>
2025-10-14 16:31:02 +0200

    Building phase

--- Optimisation/TP3/ind/solver.hpp
+++ Optimisation/TP3/ind/solver.hpp
@@ -8,195 +8,92 @@
 #include <queue>
 #include <cassert>
 
-
-template<class Vertex>
-class Solver {
-  const Graph<Vertex> &g;
-  std::unordered_set<Vertex> dominating, not_dominating, dominated, not_dominated;
-  std::vector<Vertex> vertices;
-  std::priority_queue<std::pair<int,Vertex>> heap;
+template <class Vertex>
+class Solver
+{
+    const Graph<Vertex> &g;
+    std::unordered_map<Vertex, int> independant;
+    std::vector<Vertex> vertices;
+    std::unordered_map<Vertex, int> dependant;
 
 public:
-  Solver(const Graph<Vertex> &_g) :
-    g(_g),
-    not_dominating(g.vertices()),
-    not_dominated(not_dominating),
-    vertices(not_dominating.begin(), not_dominating.end()) {
-  }
-
-  // Insert a vertex v in the dominating set, updating all sets accordingly
-  void insert_dominating(Vertex v) {
-    dominating.insert(v);
-    not_dominating.erase(v);
-    // Neighbors of v are now dominated
-    for(Vertex u : g.closedNeighbors(v)) {
-      dominated.insert(u);
-      not_dominated.erase(u);
-    }
-  }
-
-  // Tests if a vertex v is dominated using only dominating
-  // Needed to update dominated on erase_dominating
-  bool test_dominated(Vertex v) const {
-    for(Vertex u : g.closedNeighbors(v)) {
-      if(dominating.contains(u))
-        return true;
-    }
-    return false;
-  }
-
-  // Insert a vertex v in the dominating set, updating all sets accordingly
-  // Returns false if the vertex was not in the dominating set
-  bool erase_dominating(Vertex v) {
-    if(!dominating.contains(v))
-      return false;
-    dominating.erase(v);
-    not_dominating.insert(v);
-    for(Vertex u : g.closedNeighbors(v)) {
-      if(!test_dominated(u)) {
-        dominated.erase(u);
-        not_dominated.insert(u);
-      }
-    }
-    return true;
-  }
-
-  // Counts the number of vertices that would change into dominated if v is inserted into dominating
-  int count_domination(Vertex v) const {
-    int ret = 0;
-    for(Vertex u : g.closedNeighbors(v)) {
-      if(!dominated.contains(u))
-        ret++;
-    }
-
-    return ret;
-  }
-
-  Vertex choose_vertex() {
-    std::vector<Vertex> best_vertices;
-    int best_value = 0;
-
-    while(!heap.empty() &&
-        (best_vertices.empty() || heap.top().first == best_value)) {
-      auto [old_val,v] = heap.top();
-      heap.pop();
-      if (!dominating.contains(v)) {
-        int cur_val = count_domination(v);
-        if(old_val == cur_val) {
-          best_vertices.push_back(v);
-          best_value = cur_val;
+    Solver(const Graph<Vertex> &_g): 
+        g(_g),
+        independant()
+    {
+        {
+            std::unordered_set<Vertex> v = g.vertices();
+            vertices{v.begin(), v.end()};
         }
-        else {
-          auto p = std::make_pair(cur_val,v);
-          heap.push(p);
+        
+        for (auto v : vertices) {
+            dependant[v] = 0;
         }
-      }
     }
 
-    assert(!best_vertices.empty());
+    void solve_greedy()
+    {
+        auto queue = vertices;
+        std::sort(queue.begin(), queue.end(),
+                  [](Vertex& a, Vertex& b) -> int {
+                    return a.degree() - b.degree();
+                  });
+        
+        while(!queue.empty()) {
+            Vertex& v = queue.front();
 
-    Vertex ret = random_element(best_vertices);
-    for(Vertex v : best_vertices) {
-      if(v != ret) {
-        auto p = std::make_pair(best_value,v);
-        heap.push(p);
-      }
-    }
+            independant[v] = 1; // No stable neighbor by definition
+            update_neighbors(v); // Update the count (or add the neighbor)
 
-    return ret;
-  }
-
-
-  void solve_greedy() {
-    for(Vertex v : not_dominating) {
-      auto p = std::make_pair(count_domination(v),v);
-      heap.push(p);
-    }
-
-    while(not_dominated.size()) {
-      Vertex v = choose_vertex();
-      insert_dominating(v);
-    }
-  }
-
-  bool would_dominate_all(const std::vector<Vertex> &dom) const {
-    std::unordered_set<Vertex> neighbors;
-    for(Vertex v : dom) {
-      auto nv = g.closedNeighbors(v);
-      neighbors.insert(nv.begin(),nv.end());
-    }
-    for(Vertex v : not_dominated) {
-      if(!neighbors.contains(v))
-        return false;
-    }
-    return true;
-  }
-
-  // Very slow function to find the smallest set of vertices to insert to get a dominating set
-  // Only efficient if very few vertices need to be inserted
-  // A known solution is given as a parameter
-  void solve_exact(const std::unordered_set<long long int> &removed) {
-    std::unordered_set<Vertex> candidates;
-
-    for(Vertex v :not_dominated) {
-      auto nv = g.closedNeighbors(v);
-      candidates.insert(nv.begin(), nv.end());
-    }
-
-    for(size_t sz = 0; sz <= removed.size(); sz++) {
-      std::vector<std::vector<Vertex>> comb = combinations(candidates, sz);
-      std::shuffle(comb.begin(), comb.end(), rgen);
-      for(std::vector<Vertex> dom : comb) {
-        if(would_dominate_all(dom)) {
-          for(Vertex v : dom)
-            insert_dominating(v);
-          return;
+            remove_neighbors_from_queue(queue, v);
         }
-      }
     }
 
-    assert(false);
-  }
-
-
-  // Tries to decrease the size of the dominating set
-  // Returns true when succesful
-  // It will never increase the size
-  bool improve() {
-    size_t previous = dominating.size();
-    Vertex v = random_element(vertices);
-    int count = 0;
-    std::unordered_set<Vertex> removed;
-    for(Vertex u : g.closedNeighbors(v)) {
-      if(erase_dominating(u)) {
-        count++;
-        removed.insert(u);
-      }
-      if(count > 3) { // The exact solver is too slow to handle more than 4
-        break;
-      }
+    // Tries to decrease the size of the dominating set
+    bool improve()
+    {
+        for (auto [v, degree] : dependant) {
+            
+        }
     }
 
-    solve_exact(removed);
-    return dominating.size() < previous;
-  }
+    // Getter for the solution
+    const std::unordered_set<Vertex> &solution()
+    {
+        return independant;
+    }
 
+private:
+    void update_neighbors(Vertex& v) const
+    {
+        for (Vertex n : g.neighbors(v)) {
+            dependant[n] += 1;
+        }
+            
+    }
 
-  // Getter for the solution
-  const std::unordered_set<Vertex> &solution() {
-    return dominating;
-  }
+    void remove_neighbors_from_queue(std::vector<Vertex>& list, Vertex& v)
+    {
+        std::erase(
+            std::remove_if(list,
+                [&v](Vertex& n) { return g.closedneighbors(v).contains(n); }
+            ),
+            list.end()
+        );
+    }
+
 };
 
 // Save the solution to a file
-template<class Vertex>
-void save(const std::string &outfn, std::unordered_set<Vertex> solution) {
-  std::ofstream outfile(outfn);
-  for(Vertex v : solution) {
-    outfile << v << std::endl;
-  }
-  std::cout << "Saved a dominating set of size " << solution.size() << std::endl;
+template <class Vertex>
+void save(const std::string &outfn, std::unordered_set<Vertex> solution)
+{
+    std::ofstream outfile(outfn);
+    for (Vertex v : solution)
+    {
+        outfile << v << std::endl;
+    }
+    std::cout << "Saved a dominating set of size " << solution.size() << std::endl;
 }
-
 
 #endif
